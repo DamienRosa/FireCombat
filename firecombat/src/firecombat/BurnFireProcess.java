75c23b6b9cba9332bc7a09160908d4016ca6d40d
Damien Rosa <damien.guedes.r@gmail.com>
2013-11-12 23:39:33 +0000

    update burnFire process and create forestbgprocess

--- firecombat/src/firecombat/BurnFireProcess.java
+++ firecombat/src/firecombat/BurnFireProcess.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 import jadex.bridge.service.types.clock.IClockService;
 import jadex.commons.SimplePropertyObject;
 import jadex.extension.envsupport.environment.IEnvironmentSpace;
@@ -12,40 +14,48 @@ import jadex.extension.envsupport.environment.ISpaceProcess;
 import jadex.extension.envsupport.environment.space2d.Space2D;
 import jadex.extension.envsupport.math.Vector2Double;
 
-public class BurnFireProcess extends SimplePropertyObject implements ISpaceProcess{
+public class BurnFireProcess extends SimplePropertyObject implements
+		ISpaceProcess {
 
 	@Override
 	public void execute(IClockService arg0, IEnvironmentSpace arg1) {
-		Space2D env = (Space2D)arg1;
-		
+		Space2D env = (Space2D) arg1;
+
 		ISpaceObject[] space = arg1.getSpaceObjectsByType("fire");
 		Random rand = new Random();
-		for (ISpaceObject s : space) {
+		Map mapspace = new HashMap();
 
-			double x = rand.nextDouble()*env.getAreaSize().getXAsDouble();
-			double y = rand.nextDouble()*env.getAreaSize().getYAsDouble();
-			s.setProperty("position", new Vector2Double(x,y));
+		Vector2Double vec = null;
+		for (ISpaceObject s : space) {
+			/*
+			 * double x = rand.nextDouble()*env.getAreaSize().getXAsDouble();
+			 * double y = rand.nextDouble()*env.getAreaSize().getYAsDouble();
+			 * s.setProperty("position", new Vector2Double(x,y));
+			 */
+
+			vec = (Vector2Double) s.getProperty("position");
+
 		}
+
+		mapspace.put("position", new Vector2Double(vec.getXAsDouble() + 1, vec.getYAsDouble() + 1));
+		env.createSpaceObject("fire", mapspace, null);
 	}
 
 	@Override
 	public void shutdown(IEnvironmentSpace arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Override
 	public void start(IClockService arg0, IEnvironmentSpace env) {
 		Space2D space = (Space2D) env;
 		Random rand = new Random();
-		
-		double x = rand.nextDouble()*space.getAreaSize().getXAsDouble();
-		double y = rand.nextDouble()*space.getAreaSize().getYAsDouble();
-		
-		
+
+		double x = rand.nextDouble() * space.getAreaSize().getXAsDouble();
+		double y = rand.nextDouble() * space.getAreaSize().getYAsDouble();
+
 		Map map = new HashMap();
-		map.put("position", new Vector2Double(x,y));
+		map.put("position", new Vector2Double(x, y));
 		space.createSpaceObject("fire", map, null);
 	}
 
---
+++ firecombat/src/firecombat/ForestBgProcess.java
@@ -0,0 +1,45 @@
+package firecombat;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import jadex.bridge.service.types.clock.IClockService;
+import jadex.commons.SimplePropertyObject;
+import jadex.extension.envsupport.environment.IEnvironmentSpace;
+import jadex.extension.envsupport.environment.ISpaceProcess;
+import jadex.extension.envsupport.environment.space2d.Space2D;
+import jadex.extension.envsupport.math.Vector2Double;
+
+public class ForestBgProcess  extends SimplePropertyObject implements ISpaceProcess{
+
+	@Override
+	public void execute(IClockService arg0, IEnvironmentSpace arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutdown(IEnvironmentSpace arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start(IClockService arg0, IEnvironmentSpace arg1) {
+		Space2D space = (Space2D)arg1;
+		Map forest = new HashMap();
+		Random rand = new Random();
+		for (int x = 0; x < space.getAreaSize().getXAsInteger(); x++) {
+			for (int y = 0; y < space.getAreaSize().getYAsInteger(); y++) {
+				forest.put("position", new Vector2Double(x,y));
+				forest.put("probability", 0);
+				Integer r = rand.nextInt(3);
+				forest.put("density", r.toString());
+				space.createSpaceObject("forest", forest, null);
+			}
+			
+		}
+	}
+
+}
